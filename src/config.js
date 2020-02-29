4abd4f08647c1dcb82cc0def293adec97ed8c7bf
Amol Dumbre <amol.dumbre>
2020-02-29 18:40:17 +0530

    Updated config url

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://okta-login.herokuapp.com/implicit/callback',
+    redirectUri: 'https://okta-login.herokuapp.com/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
